86ff2381de7b6661e93f3a3a9a1a7bbfcc9c239f
F1shb0ne <f1shb0nes80@gmail.com>
2016-03-04 09:25:55 -0400

    added insufficient character exception for SourceFile.getChar()

---
+++ src/ca/vire/otp/exception/InsufChars.java
@@ -0,0 +1,32 @@
+package ca.vire.otp.exception;
+
+public class InsufChars extends Exception {
+
+	// Needed as super class Throwable implements Serializable 
+	private static final long serialVersionUID = -2401716239650695167L;	
+	
+	private String message = null;
+	
+	public InsufChars() {
+		super();
+	}
+
+	public InsufChars(String message) {
+		super(message);
+		this.message = message;
+	}
+	
+	public InsufChars(Throwable cause) {
+		super(cause);		
+	}
+	
+	@Override
+	public String toString() {
+		return message;
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
