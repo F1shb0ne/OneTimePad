8ed39e3d99b5954aa23ad5ade324e77ab2b9f0f8
F1shb0ne <f1shb0nes80@gmail.com>
2016-03-11 00:19:47 -0400

    rewrite of crypto, not yet finished. plenty of debug output placed everywhere

--- src/ca/vire/otp/Crypto.java
+++ src/ca/vire/otp/Crypto.java
@@ -6,36 +6,51 @@ import ca.vire.otp.exception.InsufChars;
 
 public class Crypto {
 
-	public static void encode(Pad padfile, SourceFile source, LogFile log) throws PadNotDefined, InsufChars {
+	public static void encode(Pad padFile, SourceFile source, LogFile log) throws InsufChars, PadNotDefined {
 		char inChar = 0, padChar = 0, outChar = 0;
-		boolean run = true;
-		
-		do {
+		long index = 0;
+		boolean caps;
+
+		// inform the pad file how many characters we will need
+		try {
+			padFile.reqLength(source.getFileSize());
+		} catch (PadNotDefined e) {
+			throw e;
+		}
+
+		// Keep reading source until eof is thrown
+		while (true) {
+			// Get a char from the source file
 			try {
-				// Get a char from the source file
 				inChar = source.getChar();
 			} catch (EOFException e) {
-				System.out.println("Source EOF Reached.");
-			} 
-			System.out.println("encode(): source: " + inChar);
-				
+				System.out.println("Crypto.encode(): reached source EOF.");
+				break;
+			}
+			System.out.println("Crypto.encode(): src: " + Character.toString(inChar));
+
+			// Get a char from the pad file
 			try {
-				// Get a char from the pad file
-				padChar = padfile.getChar();
-			} catch (PadNotDefined e) {				
-				throw e;
+				padChar = padFile.getChar();
 			} catch (EOFException e) {
-				System.out.println("Crypto.encode(): pad file has run out of characters.");
+				System.out.println("Crypto.encode(): pad file has run out of characters!");
 				throw new InsufChars();
 			}
-			
-			System.out.println("encode(): pad " + padChar);
-			
-		} while (run);			
+			System.out.println("Crypto.encode(): pad: " + Character.toString(padChar));
+
+
+		}
 	}
-	
+
 	public static void decode(Pad padfile, SourceFile source, LogFile log) {
-		
+
 	}
-	
+
+	private boolean isCaps(char c) {
+		if (c >= 'a')
+			return true;
+
+		return false;
+	}
+
 }
