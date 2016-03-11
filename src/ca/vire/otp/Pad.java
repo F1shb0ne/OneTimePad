a915cd1882fa22b3513377a2b9935ec246c2ded0
F1shb0ne <f1shb0nes80@gmail.com>
2016-03-10 20:05:31 -0400

    too many changes! not enough commits! made fields private, no need of them being public. had issue with pad files being stored with absolute path, not relative path. pad now uses a temp file to pre-buffer usable charactes from the pad file; if the pad file doesnt have enough remaining characters it'll abort and reset pad index.

--- src/ca/vire/otp/Pad.java
+++ src/ca/vire/otp/Pad.java
@@ -1,6 +1,7 @@
 package ca.vire.otp;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,132 +16,199 @@ import ca.vire.otp.exception.PadNotDefined;
  * It's purpose is to return characters in the pad file and keep track
  * of where it's pulling them from.
  */
-public final class Pad {	
-	private String pwd = System.getProperty("user.dir");
-	private String padFilename = null;
+public final class Pad {
 	private String padFileURI = null;
-	private RandomAccessFile raf;
-	private long charsRead = 0;
-	
-	public long fileLength;
-	public long padIndex;
-	public boolean isDefined = false;
-	
+	private String tempFileURI = null;
+	private RandomAccessFile padReader = null;
+	private FileReader tempReader = null;
+	private long fileLength = 0;
+	private long tempFileLength = 0;
+	private long padIndex;
+	private long tempIndex;
+	private boolean isDefined = false;
+	private boolean isTempReady = false;
+
 	/*
 	 * Open the pad file and move seek position to where it last was.
 	 */
-	public Pad(String padfilename, long index) {
-		padFilename = padfilename;
-		//padFileURI = pwd + "/" + padFilename;
+	public Pad(String padfilename, long index) throws FileNotFoundException {
 		padFileURI = padfilename;
+		tempFileURI = padFileURI + ".tmp";
 		padIndex = index;
-		
+
 		try {
-			raf = new RandomAccessFile(new File(padFileURI), "r");
-			fileLength = raf.length();
-			isDefined = true;
-			raf.seek(padIndex);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
+			padReader = new RandomAccessFile(new File(padFileURI), "r");
+			padReader.seek(padIndex);
+			fileLength = padReader.length();
 		} catch (IOException e) {
-			e.printStackTrace();			
-			System.exit(1);
-		}
-	}
-	
-	/*
-	 * create a temporary file containing all the characters needed specified by
-	 * the lengthNeeded argument. This is done because otp only operates on the
-	 * letters a-z: all other characters are ignored. 
-	 */
-	public void prepChars(long lengthNeeded) throws InsufChars, PadNotDefined {
-		long i = 0;
-		int c = 0;
-		FileWriter writer = null;
-		
-		String tempFile = padFileURI + ".tmp";
-		
-		// Open the temporary file
-		try {
-			writer = new FileWriter(new File(tempFile));
-		} catch (IOException e) {			
 			e.printStackTrace();
 			System.exit(1);
 		}
 
-		// 
-		do {
-			try {
-				c = getChar();
-			} catch (PadNotDefined e) {
-				throw e;
-			} catch (EOFException e) {
-				throw new InsufChars();
-			}
+		isDefined = true;
+	}
 
-			try {
-				if (isLetter(c)) {
-					writer.write(toUCase(c));
+	/*
+	 * Request a given number of characters and write them to a temporary file.
+	 * If the request fails, throw InsufChars exception and reset pad index.
+	 */
+	public void reqLength(long lengthNeeded) throws InsufChars, PadNotDefined {
+		long i = 0;
+		long originalPadIndex = padIndex;
+		char c = 0;
+		FileWriter writer = null;
+
+		System.out.println("Pad.reqLength(): was requested " + Long.toString(lengthNeeded) + " characters");
+
+		// Open the temporary file
+		System.out.println("Pad.reqLength(): opening temp file: " + tempFileURI);
+		try {
+			writer = new FileWriter(new File(tempFileURI));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// write the temporary file
+		do {
+			// read pad until a letter is found
+			do {
+				try {
+					c = rawGetChar();
+					System.out.println("Pad.reqLength(): Read: " + Character.toString(c));
+				} catch (EOFException e) {
+					padIndex = originalPadIndex;
+					throw new InsufChars();
 				}
+			} while (!isLetter(c));
+
+			// write it to the temporary file
+			try {
+				writer.write(toUCase(c));
+				System.out.println("Pad.reqLength(): wrote: " + c);
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.exit(1);
 			}
-			
 		} while (++i < lengthNeeded);
-		
+		System.out.println("Pad.reqLength(): Got enough; closing temp file.");
+
+		// close the temp file
 		try {
 			writer.close();
-		} catch (IOException e) {
-			
-		}
-		
-		
-	}
-	
-	private boolean isLetter(int c) {
-		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-			return true;
-		
-		return false;
-	}
-	
-	// Assumes given value already passes isLetter()
-	private int toUCase(int c) {
-		if (c >= 97)
-			return c - 32;
-		
-		return c;
-	}
-	
-	// Assumes given value already passes isLetter()
-	private boolean isUCase(int c) {
-		if (c <= 90)
-			return true;
-		
-		return false;
-	}
-	
-	/*
-	 * Read one character from the pad file.
-	 * Does not touch the padIndex
-	 */
-	public char getChar() throws EOFException, PadNotDefined {
-		long charsRead = 0;
-		char c = 0;
-
-		// Bail immediately if nothing can be read
-		if (!isDefined)
-			throw new PadNotDefined();
-		
-		try {
-			c = raf.readChar();
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
+		// update the temp file length
+		tempFileLength = lengthNeeded;
+		isTempReady = true;
+	}
+
+	public boolean isDefined() {
+		return isDefined;
+	}
+
+	private boolean isLetter(int c) {
+		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+			return true;
+
+		return false;
+	}
+
+	// Assumes given value already passes isLetter()
+	private char toUCase(char c) {
+		if (c >= 97)
+			return (char)(c - 32);
+
+		return c;
+	}
+
+	// Assumes given value already passes isLetter()
+	private boolean isUCase(int c) {
+		if (c <= 90)
+			return true;
+
+		return false;
+	}
+
+	/*
+	 * Read characters back from the temp file, one at a time.
+	 */
+	public char getChar() throws EOFException {
+		char c = 0;
+
+		// should not be here if isTempReady is false
+		if (!isTempReady) {
+			System.out.println("Pad.getChar(): temp file not defined!");
+			throw new EOFException();
+		}
+
+		// open the temp file if it hasn't been already
+		if (isTempReady && tempReader == null) {
+			try {
+				System.out.println("Pad.getChar(): opening temp file.");
+				tempReader = new FileReader(new File(tempFileURI));
+			} catch (FileNotFoundException e) {
+				// This should never happen
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+
+		// are there more characters to read?
+		if (tempIndex < tempFileLength) {
+			try {
+				c = (char) tempReader.read();
+				++tempIndex;
+				System.out.println("Pad.getChar(): read: " + Character.toString(c));
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		} else {
+			// reached end of temp file, close up
+			try {
+				tempReader.close();
+				System.out.println("Pad.getChar(): closing temp file.");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			isTempReady = false;
+			throw new EOFException();
+		}
+
+		return c;
+	}
+
+	/*
+	 * Read one character from the pad file.
+	 */
+	public char rawGetChar() throws EOFException, PadNotDefined {
+		char c = 0;
+
+		// Bail immediately if nothing can be read
+		if (!isDefined) {
+			System.out.println("Pad.rawGetChar(): pad file not defined!");
+			throw new PadNotDefined();
+		}
+
+		if (padIndex < fileLength) {
+			try {
+				c = (char) padReader.read();
+				++padIndex;
+				System.out.println("Pad.rawGetChar(): read : " + Character.toString(c));
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		} else {
+			System.out.println("Pad.rawGetChar(): reached end of file.");
+			throw new EOFException();
+		}
+
 		return c;
 	}
 
